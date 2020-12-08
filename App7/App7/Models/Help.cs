719c46e59596300c4aababdbc95a4a304e95841c
Callam Barfoot <c.m.d.barfoot1@newcastle.ac.uk>
2020-12-08 15:43:42 +0000

    Add project files.

---
+++ App7/App7/Models/Help.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace App7.Models
+{
+    public class Help
+    {
+        public string Filename { get; set; }
+        public string Text { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
