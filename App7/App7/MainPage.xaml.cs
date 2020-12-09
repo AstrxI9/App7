2fda6c18bd181cfa9e0b6e0caddc894d5d701b8d
Callam Barfoot <c.m.d.barfoot1@newcastle.ac.uk>
2020-12-09 10:13:30 +0000

    09/12/20 commit

--- App7/App7/MainPage.xaml.cs
+++ App7/App7/MainPage.xaml.cs
@@ -13,11 +13,41 @@ namespace App7
         public MainPage()
         {
             InitializeComponent();
+            MyMenu = GetMenus();
+            this.BindingContext = this;
         }
+
+        public List<Menu> MyMenu { get; set; }
+
+        private List<Menu> GetMenus()
+        {
+            return new List<Menu>
+            {
+                new Menu{Name = "Home", Icon= "logoengie.png"},
+                new Menu{Name = "Help", Icon= "logoengie.png"},
+                new Menu{Name = "About", Icon= "logoengie.png"},
+            };
+        }
+
 
         private void OpenSwipe(object sender, EventArgs e)
         {
-
         }
+
+        private void CloseSwipe(object sender, EventArgs e)
+        {
+        }
+
+        private void OnButtonClicked(object sender, EventArgs e)
+        {
+            (sender as Button).Text = "Button Clicked";
+        }
+    }
+
+    public class Menu
+    {
+        public string Name { get; set; }
+
+        public string Icon { get; set; }
     }
 }
